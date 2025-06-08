dc7446c2f03a0826bb0b9fd84ac6d686c25fcbc4
yoonseo-han <yooncer00@gmail.com>
2025-06-08 13:34:33 +0000

    Get PID and pring

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello wORL")
+	if len(os.Args) < 2 {
+		fmt.Println("More commands needed")
+		os.Exit(1)
+	}
+
+	switch os.Args[1] {
+	case "run":
+		run()
+	}
+}
+
+func run() {
+	fmt.Printf("Funning %v as PID %d\n", os.Args[2:], os.Getpid())
 }
