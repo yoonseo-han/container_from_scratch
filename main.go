f29f845b2ad44be007da9ba1e8cda59436430ec5
yoonseo-han <yooncer00@gmail.com>
2025-06-09 15:42:24 +0000

    Separate namespace of PID, Mount, Network and IPC of container process

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"syscall"
 )
 
 func main() {
@@ -15,6 +16,9 @@ func main() {
 	switch os.Args[1] {
 	case "run":
 		run()
+	default:
+		fmt.Printf("Unknown command: %s\n", os.Args[1])
+		os.Exit(1)
 	}
 }
 
@@ -22,6 +26,15 @@ func run() {
 	fmt.Printf("Running %v as PID %d\n", os.Args[2:], os.Getpid())
 
 	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS | // Hostname
+			syscall.CLONE_NEWPID | // Process IDs
+			syscall.CLONE_NEWNS | // Mount points
+			syscall.CLONE_NEWNET | // Network
+			syscall.CLONE_NEWIPC, // IPC
+	}
+
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
